34fc2d4375491265aa0f5b94ba0a2fa67c270a9d
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-02-14 14:44:12 -0500

    Ejemplos condiciones

---
+++ script.js
@@ -0,0 +1,25 @@
+// var edad=prompt("ingrese edad")
+
+
+// if(edad == 18 ){
+// console.log("Esto es verdad...");
+// }
+
+
+// var edad =prompt("Ingrese su edad")
+// if(edad >= 18){
+//     console.log("Es mayor de edad...")
+// } else {
+//     console.log("No es mayor de edad....")
+// }
+
+
+
+// var edad =prompt("Ingrese su edad")
+// if(edad < 18){
+//     console.log("Es menor de edad...")
+// } else if (edad > 18 ) {
+//     console.log("Es mayor de edad....")
+// } else {
+//     console.log("No valido...")
+// }
